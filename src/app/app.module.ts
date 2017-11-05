f386c2c2a398759d2920a8d0246edafd8767800e
Oana Vezentan <oana.vezentan@gmail.com>
2017-11-05 14:07:18 +0100

    Added images and currency to products using data binding and displayed the data using pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
@@ -12,10 +12,11 @@ import { ProductListComponent } from './products/product-list.component';
   ],
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   exports: [
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
